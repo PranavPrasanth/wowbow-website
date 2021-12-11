a2a9e3d0773ecd9b2d2c87543527f83b8cdd3747
PranavPrasanth <94633279+PranavPrasanth@users.noreply.github.com>
2021-12-11 23:07:50 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,19 +1,43 @@
 <?php
-//get data from form  
 
-$name = $_POST['First Name'];
-$lname = $_POST['Last Name'];
-$email= $_POST['Email'];
-$phone = $_POST['phone'];
-$message= $_POST['Message'];
-$to = "pranavvv6632@gmail.com";
-$subject = "Mail From wow-bow";
-$txt ="First Name = ". $name . "\r\nLst Name = ".$lname . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@wowbow.co.in" . "\r\n" .
-"CC: roundrobin450@gmail.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
+//get data from form  
+    $message_sent = false;
+    if{isset(s_POST['Email']) && $_POST['Email'] !=''){
+        if{filter_var($_POST['Email'], FILTER_VALIDATE_EMAIL)){
+
+
+            $fname = $_POST['First Name'];
+            $lname = $_POST['Last Name'];
+            $email= $_POST['Email'];
+            $phone = $_POST['phone'];
+            $message= $_POST['Message'];
+            $to = "pranavvv6632@gmail.com";
+            $subject = "Mail From wow-bow";
+            
+            
+            $body = "";
+            
+            
+            $body .="First Name: ".$fname. "\r\n" 
+            $body.="Last Name: ".$lname. "\r\n"
+            $body.="Email: ".$Email.  "\r\n"  
+            $body.="phone: ".$phone. "\r\n"
+            $body.="Message: ".$message. "\r\n"
+            $headers = "From: noreply@wowbow.co.in" . "\r\n" .
+            "CC: roundrobin450@gmail.com";
+            
+             mail($to,$subject,$,$body);
+            
+             $message_sent=true;
+            
+                                             
+        
+
+        }
+        else{
+            $invalid_class_name = "form-invalid";
+         }
+     }
 //redirect
-header("Location:thankyou.html");
+
 ?>
