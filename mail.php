dd8cbf86c7690d6e1e60671352dcbea24d0a229b
PranavPrasanth <94633279+PranavPrasanth@users.noreply.github.com>
2021-12-13 20:57:52 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form  
+$fname = $_POST['Firstname'];
+$lname = $_POST['Lastname'];
+$email= $_POST['emailaddress'];
+$number= $_POST['mobile'];
+$message= $_POST['message'];
+$to = "pranavvv1@gmail.com";
+$subject = "Mail From wowbow";
+$txt ="Name = ". $fname .$lname . "\r\n  Email = " . $email . "\r\n Phone=" .$number . "\r\n Message =" . $message;
+$headers = "From: wowbow.co.in" . "\r\n" .
+"CC: roundrobin450@gmai.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
